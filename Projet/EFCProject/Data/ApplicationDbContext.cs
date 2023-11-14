b53554c735bfb275b47c5eee8ab4bb7ddbb4c140
achilgourier <achil@sfr.fr>
2023-11-14 15:43:00 +0100

    Set Up
    model MVC + bdd

---
+++ Projet/EFCProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using EFCProject.Models;
+
+namespace EFCProject.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<EFCProject.Models.Game>? Game { get; set; }
+    }
+}
---
+++ Projet/EFCProject/Models/Game.cs
@@ -0,0 +1,31 @@
+﻿using System.Drawing;
+
+
+
+namespace EFCProject.Models
+{
+    public class Game
+    {
+
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? Studio { get; set; }
+        public string? Support { get; set; }
+        public float Size { get; set; }    
+        public int Score { get; set; }
+        public string? Engine { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public float Budget { get; set; }
+        public string? Statut { get; set; }
+        public string? Type { get; set; }
+
+
+        public Game()
+        {
+            
+        }
+
+    }
+}
